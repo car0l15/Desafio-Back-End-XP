6125120655bf47495ab53c9d7cf534c45b8ff34f
Caroline Nepomuceno <carol14898@gmail.com>
2022-07-24 11:58:25 -0300

    ajustes no Joi

--- src/middleware/JoiInvestiment.ts
+++ src/middleware/JoiInvestiment.ts
@@ -12,8 +12,8 @@ const investimentoDTO = Joi.object({
     .required(),
 }).messages({
   'any.required': '{(#label)} é um campo obrigatório',
-  'any.min': '{(#label)} valor minimo deve ser igual a 1',
-  'any.number': '{(#label)} deve ser do tipo number',
+  'number.min': '{(#label)} valor minimo deve ser igual a 1',
+  'number.base': '{(#label)} deve ser do tipo number',
 });
 
 const investimentoValidation = (req: Request, res: Response, next: NextFunction) => {
@@ -23,19 +23,17 @@ const investimentoValidation = (req: Request, res: Response, next: NextFunction)
     return next();
   }
 
-  //   console.log(error.message, 'error');
+  console.log(error.message, 'error');
   if (error.message.includes('obrigatório')) {
     return res.status(400).json({ message: error.message });
   }
 
-  if (error.message.includes('equal to 1')) {
-    return res.status(422).json({ message:
-    'todos os campos devem ter seu valor maior ou igual a 1',
-    });
+  if (error.message.includes('valor minimo deve ser igual')) {
+    return res.status(422).json({ message: error.message });
   }
 
-  if (error.message.includes('a number')) {
-    return res.status(422).json({ message: 'todos os campos devem ser numéricos' });
+  if (error.message.includes('tipo number')) {
+    return res.status(422).json({ message: error.message });
   }
 
   return res.status(422).json({ message: error.message });
---
+++ src/middleware/JoiValidateConta.ts
@@ -0,0 +1,7 @@
+// const contaDTO = Joi.object({
+//   codCliente: Joi.number().integer().options({ convert: false }).label('codCliente')
+//     .min(1)
+//     .required(),
+//   valor: Joi.number().options({ convert: false }).label('valor').min(1)
+//     .required(),
+// });
